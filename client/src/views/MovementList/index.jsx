4e7c3b1b37e14fa966cbf02c5969ef1483b7c551
Ricardo <ricardotomaz027rgmail.com>
2020-03-19 11:32:36 +0000

    cards

--- client/src/views/MovementList/index.jsx
+++ client/src/views/MovementList/index.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { list } from '../../services/movement';
 import './style.scss';
+import { Card , Button} from 'react-bootstrap';
 
 class MovementList extends Component {
   constructor(props) {
@@ -33,9 +34,21 @@ class MovementList extends Component {
         <div className="movement__list">
           {/* <pre>{JSON.stringify(this.state.movements,null, 2)}</pre> */}
           {this.state.movements.map(movement => (
-            <Link to={`movement/${movement._id}`} key={movement._id}>
+            <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title text = 'primary'>{movement.name}</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
+          
+           /* <Link to={`movement/${movement._id}`} key={movement._id}>
               <p>{movement.name}</p>
-            </Link>
+            </Link>*/
           ))}
         </div>
       </div>
