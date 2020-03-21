ccb71d413cb9ac9eda59859527e30cf227e97b24
FilipeMorais43 <fmoraisyou@gmaile.com>
2020-03-21 19:33:15 +0100

    searchbar style

--- client/src/components/SearchBox/index.jsx
+++ client/src/components/SearchBox/index.jsx
@@ -1,35 +1,35 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import "./styles.scss";
+import './styles.scss';
 
 class SearchBox extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      search: ""
+      search: ''
     };
     this.startSearch = this.startSearch.bind(this);
   }
 
   startSearch(event) {
-    
     const search = event.target.value;
     this.setState({
       search
     });
-    
+
     this.props.search(search);
   }
 
   render() {
-    console.log("inside serach", this.state.search)
+    console.log('inside serach', this.state.search);
     return (
-      <div className="search-box">
+      <div>
         <form>
-          <span>Search:</span>
           <input
+            className="search-box"
             type="search"
             name="search"
+            placeholder="Search..."
             value={this.state.search}
             onChange={this.startSearch}
           />
